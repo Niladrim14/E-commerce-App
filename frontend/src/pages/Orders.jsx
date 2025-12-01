527fd4bf650ef6678af1d4a7591bb5f6c359ab41
Nilarim14 <niladrim004@gmail.com>
2025-12-01 20:17:06 +0530

    PlaceOrder Jsx done

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {ShopContext} from '../context/ShopContext'
+import Title from '../components/Title' 
+
 
 const Orders = () => {
+ const {products ,cartItems ,getTotalCartAmount,currency} = useContext(ShopContext)
   return (
-    <div>Orders</div>
+    <div>
+      <div className='text-2xl mb-3 mt-14'>
+    <Title text1={"Your"} text2={"Orders"}/>
+      </div>
+      <div >
+         {
+          products.slice(1,4).map((item,index)=>(
+             <div  key ={index} className='py-4 border-b border-t text-gray-400 flex flex-col md:flex-row  md:flex  md:items-center justify-between gap-6 shadow-xl '>
+              <div className='flex items-start gap-6 text-sm '>
+                <img className='w-16 sm:w-20' src={item.image[0]} alt={item.name} />
+                <div>
+                  <p className='sm:text-base font-medium text-gray-800'>{item.name}</p>
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-700 '>
+                    <p className='text-m'>{currency}{item.price}</p>
+                    <p>Quantity-1</p>
+                    <p>Size M</p>
+                  </div>
+                  <p className='mt-2'>Date: <span className='text-gray-400'>24.July</span></p>
+                </div>
+
+              </div>
+
+             </div>
+          ))
+         }
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -3,9 +3,12 @@ import {ShopContext} from '../context/ShopContext'
 import { useContext } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 
 const PlaceOrder = () => {
+  const [method ,setmethod] = React.useState('cod')
+  const {navigate} = useContext(ShopContext)
 
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t px-2 sm:px-0'>
@@ -14,22 +17,21 @@ const PlaceOrder = () => {
           <Title text1={"DELIVERY"} text2={"INFORMATION"}/>
         </div>
         <div className='flex gap-3'>
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="First Name" required />
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Middle Name" />
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Surname" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="First Name" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Middle Name" />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Surname" required />
         </div>
-         <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="email" placeholder="Email address" required />
-         <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Street Address" required />
-
+         <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="email" placeholder="Email address" required />
+         <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Street Address" required />  
           <div className='flex gap-3'>
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="City" required />
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="State/Province" required />
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Postal Code" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="City" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="State/Province" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Postal Code" required />
         </div>
 
          <div className='flex gap-3'>
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Phone Number" required />
-          <input className='border border-gray-600 py-1.5 px-3.5 w-full' type="text" placeholder="Country" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Phone Number" required />
+          <input className='border border-gray-400 py-1.5 px-3.5 w-full' type="text" placeholder="Country" required />
         </div>
       </div>
       
@@ -38,9 +40,29 @@ const PlaceOrder = () => {
         <div className='mt-8 min-w-80'>
         <CartTotal/>
         </div>
+        <div className='mt-12'>
+          <Title text1={"PAYMENT"} text2={"METHOD"}/>
+            <div className='flex gap-3 flex-col lg:flex-row'>
+
+              <div  onClick={()=>setmethod('razorpay')} className='flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-500' : ''} `}></p>  
+              <img  className='h-5 mx-4' src={assets.razorpay_logo}  />
+              </div>
+                 <div  onClick={()=>setmethod('stripe')} className='flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-500 ' : ''} `}></p> 
+                <img  className='h-5 mx-4' src={assets.stripe_logo}  />
+              </div>
+                 <div  onClick={()=>setmethod('cod')}  className='flex items-center gap-3 border border-gray-300   p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-500' : ''} `}></p>  
+                      <p className='text-gray-600'>Cash on Delivery</p>
+              </div>
+            </div>
+            <div>
+          <button onClick={()=>navigate('/orders')} className='bg-pink-600 text-white w-full py-3 mt-6 hover:bg-pink-700  font-semibold  transition-colors'>PLACE ORDER</button>
+            </div>
+        </div>
 
       </div>
-
 
     </div>
   )
